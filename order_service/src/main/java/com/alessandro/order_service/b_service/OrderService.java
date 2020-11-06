1b6a6736189bb316abefb0af9fa210e807773f19
Alessandro <alessandro.molinaro96@gmail.com>
2020-11-06 21:55:51 +0100

    modificato OrderService per utilizzare la comunicazione asincrona

--- order_service/src/main/java/com/alessandro/order_service/b_service/OrderService.java
+++ order_service/src/main/java/com/alessandro/order_service/b_service/OrderService.java
@@ -3,40 +3,29 @@ package com.alessandro.order_service.b_service;
 import com.alessandro.order_service.c_repository.OrderRepository;
 import com.alessandro.order_service.d_entity.Order;
 import com.alessandro.order_service.d_entity.OrderLine;
+import com.alessandro.order_service.messaging.dto.MessageCustomerCheck;
+import com.alessandro.order_service.messaging.rabbitmq.config.MessagingConfig;
 import com.alessandro.order_service.support.exception.OrderException;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 
 @Service
 public class OrderService {
 
-    @Value("${customerService.ip}")
-    private String ipCustomer;
-
-    @Value("${productService.ip}")
-    private String ipProduct;
-
-    @Value("${customerService.port}")
-    private String portCustomer;
-
-    @Value("${productService.port}")
-    private String portProduct;
-
     @Autowired
     OrderRepository orderRepository;
 
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    public List<Order> customerOrder(int idCustomer){
+        return orderRepository.findByIdCliente(idCustomer);
+    }
 
     /**
      * @param order
@@ -44,100 +33,34 @@ public class OrderService {
      * @throws OrderException
      */
     @Transactional(readOnly = false)
-    public Order create(Order order) throws OrderException {//TODO Test --> tested
+    public Order create(Order order) throws OrderException {
+        order.setState("SOSPESO");
+        order.setMessageState("ordine in sospeso");
 
-        Map<Integer, Integer> backupProdottoQta = new HashMap<>();
-        try {
-            Order ret = new Order();
-            setCustomer(ret, order.getIdCliente());
-            for (OrderLine ol : order.getLineaOrdine()) {
-                setProduct(ol,backupProdottoQta);
-                ol.setOrdine(ret);
-                ret.getLineaOrdine().add(ol);
-            }
-            orderRepository.save(ret);
-            return ret;
-        }catch (OrderException e){
-            if(!backupProdottoQta.isEmpty()){
-                RestTemplate restTemplate = new RestTemplate();
-                for(int idP : backupProdottoQta.keySet()){
-                    restTemplate.put("http://{ip}:{port}/product_service/product/{idProdotto}/update_qta/{qta}",
-                                        null, this.ipProduct, this.portProduct, idP, backupProdottoQta.get(idP)
-                                    );
-                }
-            }
-            throw new OrderException(e.getMessage());
+        //calcolo totale parziale di ogni linea ordine e determino il totale dell'ordine
+        double totaleOrdine = 0;
+        for(OrderLine ol : order.getLineaOrdine()){
+            ol.setOrdine(order);
+
+            double totaleOL = ol.getPrezzoProdotto()*ol.getQta();
+            totaleOL = Math.floor(totaleOL*100)/100; //prelevo le prime due cifre decimali
+            ol.setTotale(totaleOL);
+            totaleOrdine += totaleOL;
         }
+        totaleOrdine = Math.floor(totaleOrdine*100)/100;//prelevo le prime due cifre decimali
+        order.setTotale(totaleOrdine);
+
+        //salvo ordeine temporaneo
+        orderRepository.save(order);
+
+        //invio messaggio di verifica del cliente
+        rabbitTemplate.convertAndSend(
+                MessagingConfig.EXCHANGER_ORDER_SERVICE_NAME,
+                MessagingConfig.ROUTINGKEY_CHECK_CUSTOMER_NAME,
+                new MessageCustomerCheck(order.getIdCliente(), order.getId(), totaleOrdine)
+        );
+        return order;
     }
 
-    /**
-     * effettua la richiesta al servizio che gestisce i prodotti per ottenere i dati relativi al prodotto verificando
-     * che la quantità richiesta è effettivamente disponibile
-     * @param ol
-     * @throws OrderException
-     */
-    private void setProduct(OrderLine ol, Map<Integer, Integer> bk) throws OrderException {
-        RestTemplate restTemplate = new RestTemplate();
 
-        String JsonProduct;
-        try {
-            JsonProduct = restTemplate.getForObject("http://{ip}:{port}/product_service/product/{id}", 
-                                                    String.class, this.ipProduct, this.portProduct, ol.getIdProdotto()
-                                                    );
-        }catch (HttpClientErrorException e){
-            throw new OrderException("errore nel reperire i dati del prdotto "+ol.getIdProdotto());
-        }
-        System.out.println("prelevato prodotto "+JsonProduct);
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        try {
-            JsonNode rootNode = objectMapper.readTree(JsonProduct);
-            ol.setNomeProdotto(rootNode.path("name").asText());
-            if(ol.getQta() < 0)
-                throw new OrderException(String.format("la quantità richiesta per il prodotto %d non è ammessa",ol.getIdProdotto()));
-            if(ol.getQta().compareTo( rootNode.path("qta").asInt() ) > 0)
-                throw new OrderException("la quantità disponibile del prodotto "+ ol.getNomeProdotto()+" è inferiore a quella richiesta");
-            bk.put(ol.getIdProdotto(),ol.getQta());
-            restTemplate.put("http://{ip}:{port}/product_service/product/{idProdotto}/update_qta/{qta}",
-                             null, this.ipProduct, this.portProduct, ol.getIdProdotto(), ol.getQta()*-1
-                            );
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-            throw new OrderException("");
-        }
-    }
-
-    /**
-     * Effettua una richiesta al servizio dei clienti per ottenere i dati relativi al cliente che ha effettuato l'ordine
-     * @param o
-     * @param idCliente
-     * @throws OrderException
-     */
-    private void setCustomer(Order o, int idCliente) throws OrderException {
-        RestTemplate restTemplate = new RestTemplate();
-        String JsonCustomer;
-        try{
-            JsonCustomer = restTemplate.getForObject("http://{ip}:{port}/customer_service/customer/{id}", 
-                                                        String.class, this.ipCustomer, this.portCustomer,idCliente
-                                                    );
-        }catch (HttpClientErrorException e){
-            throw new OrderException("errore nel reperire i dati del cliente "+idCliente);
-        }
-        System.out.println("prelevato cliente"+JsonCustomer);
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        try {
-            JsonNode rootNode = objectMapper.readTree(JsonCustomer);
-            o.setNomeCliente(rootNode.path("nome").asText());
-            o.setCognomeCliente(rootNode.path("cognome").asText());
-            o.setIdCliente(idCliente);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-            throw new OrderException("");
-        }
-    }
-
-    public List<Order> customerOrder(int idCustomer){
-        return orderRepository.findByIdCliente(idCustomer);
-    }
 }
